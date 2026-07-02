640345204b4de9a63b9813bf23cf76c8a74b3e20
Carlo Satta <sattacarlo@gmail.com>
2026-07-02 09:20:30 +0200

    seed all payload field types in dev
    - dev seeder now creates integer, decimal, string, boolean, datetime and text fields
    - write the right value column per type
    - helps test the export path for every eav type

--- src/database/seeders/DevSeeder.php
+++ src/database/seeders/DevSeeder.php
@@ -43,15 +43,40 @@ class DevSeeder extends Seeder
 
     private function createPayloadFields(Version $version)
     {
-        return collect(['score', 'level'])->mapWithKeys(function (string $code) use ($version) {
-            $field = PayloadField::factory()->create([
-                'version_id' => $version->id,
-                'code' => $code,
-                'label' => ucfirst($code),
-            ]);
+        $definitions = [
+            ['code' => 'score', 'data_type' => 'integer'],
+            ['code' => 'level', 'data_type' => 'integer'],
+            ['code' => 'difficulty_rating', 'data_type' => 'decimal'],
+            ['code' => 'language', 'data_type' => 'string'],
+            ['code' => 'utm_source', 'data_type' => 'string'],
+            ['code' => 'is_winner', 'data_type' => 'boolean'],
+            ['code' => 'last_action_at', 'data_type' => 'datetime'],
+            ['code' => 'note', 'data_type' => 'text'],
+        ];
 
-            return [$code => $field];
+        return collect($definitions)->mapWithKeys(function (array $definition) use ($version) {
+            $field = PayloadField::factory()->create(array_merge([
+                'version_id' => $version->id,
+                'code' => $definition['code'],
+                'label' => ucfirst(str_replace('_', ' ', $definition['code'])),
+                'data_type' => $definition['data_type'],
+            ], $this->flagsFor($definition['data_type'])));
+
+            return [$definition['code'] => $field];
         });
+    }
+
+    private function flagsFor(string $dataType)
+    {
+        if ($dataType === 'text') {
+            return ['is_filterable' => false, 'is_sortable' => false, 'is_aggregatable' => false];
+        }
+
+        if ($dataType === 'string') {
+            return ['is_filterable' => true, 'is_sortable' => true, 'is_aggregatable' => false];
+        }
+
+        return ['is_filterable' => true, 'is_sortable' => true, 'is_aggregatable' => true];
     }
 
     private function createEvents(Version $version, Player $player, VersionPlayer $versionPlayer, $fields)
@@ -61,14 +86,49 @@ class DevSeeder extends Seeder
             ->create($this->owner($version, $player, $versionPlayer))
             ->each(function (Event $event) use ($version, $fields) {
                 foreach ($fields as $field) {
-                    PayloadValue::factory()->create([
+                    PayloadValue::factory()->create(array_merge([
                         'version_id' => $version->id,
                         'payload_field_id' => $field->id,
                         'entity_type' => 'event',
                         'entity_id' => $event->id,
-                    ]);
+                    ], $this->valueFor($field->data_type)));
                 }
             });
+    }
+
+    private function valueFor(string $dataType)
+    {
+        $columns = [
+            'value_string' => null,
+            'value_integer' => null,
+            'value_decimal' => null,
+            'value_boolean' => null,
+            'value_datetime' => null,
+            'value_text' => null,
+        ];
+
+        switch ($dataType) {
+            case 'integer':
+                $columns['value_integer'] = rand(1, 1000);
+                break;
+            case 'decimal':
+                $columns['value_decimal'] = rand(0, 10000) / 100;
+                break;
+            case 'boolean':
+                $columns['value_boolean'] = (bool) rand(0, 1);
+                break;
+            case 'datetime':
+                $columns['value_datetime'] = now()->subDays(rand(0, 30))->format('Y-m-d H:i:s');
+                break;
+            case 'text':
+                $columns['value_text'] = 'Note '.rand(1, 9999);
+                break;
+            default:
+                $columns['value_string'] = ['it', 'en', 'es', 'google', 'facebook', 'organic'][array_rand(['it', 'en', 'es', 'google', 'facebook', 'organic'])];
+                break;
+        }
+
+        return $columns;
     }
 
     private function owner(Version $version, Player $player, VersionPlayer $versionPlayer)
